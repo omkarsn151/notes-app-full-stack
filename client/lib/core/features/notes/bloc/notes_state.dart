e21f9af7f361269129fd0ed68aca3404f29724c3
omkarsn151 <omkarsnimbalkar1501@gmail.com>
2025-05-12 10:53:28 +0530

    notes state

---
+++ client/lib/core/features/notes/bloc/notes_state.dart
@@ -0,0 +1,33 @@
+import 'package:client/core/features/notes/data/notes_model.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class NotesState extends Equatable {
+  const NotesState();
+  @override
+  List<Object?> get props => [];
+} 
+
+class NotesInitial extends NotesState {}
+
+class NotesLoading extends NotesState {}
+
+class NotesLoaded extends NotesState {
+  final List<NotesModel> notes;
+  const NotesLoaded(this.notes);
+  @override
+  List<Object?> get props => [notes];
+}
+
+class NotesError extends NotesState {
+  final String message;
+  const NotesError(this.message);
+  @override
+  List<Object?> get props => [message];
+}
+
+class NotesException extends NotesState {
+  final String message;
+  const NotesException(this.message);
+  @override
+  String toString() => message;
+}
